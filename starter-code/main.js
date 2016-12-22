c73de882dae56df17f7374a5aedf4bf67dffd073
Sabrina <sjiang11@gmail.com>
2016-12-21 19:02:44 -0800

    created pop up alert

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,2 +1,10 @@
 console.log("JS file is connected to HTML! Woo!");
 var cardOne="queen", cardTwo="queen", cardThree="king", cardFour="king";
+
+if(cardOne === cardTwo){
+	alert("You found a match!");
+}
+else{
+	alert("Sorry, try again.");
+}
+
