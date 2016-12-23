174dd201dad02735b830ebd4e3fddab94c34365f
Sabrina <sjiang11@gmail.com>
2016-12-22 17:39:08 -0800

    cards in JS

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,10 +1,19 @@
 console.log("JS file is connected to HTML! Woo!");
 var cardOne="queen", cardTwo="queen", cardThree="king", cardFour="king";
 
-if(cardOne === cardTwo){
+/**if(cardOne === cardTwo){
 	alert("You found a match!");
 }
 else{
 	alert("Sorry, try again.");
-}
+}*/
 
+var newGameBoard = document.getElementById('game-board');
+
+var createCards = function(){
+	for(var i = 0; i < 4; i++){
+		var newCard = document.createElement('div');
+		newCard.className = 'card';
+		newGameBoard.appendChild(newCard);
+	}
+}
